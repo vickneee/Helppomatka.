92ba0f6bfae599e5c8317cb02b2013d6acbe7f7a
vickneee <victoria.vavulina@outlook.com>
2024-01-24 20:37:55 +0200

    Merge conflict cleaning

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,21 +1,12 @@
 import Navbar from "../../components/navbar/Navbar"
 import Header from "../../components/header/Header"
 import "./home.css"
-<<<<<<< HEAD
-=======
-
->>>>>>> ahmed
-
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Header />
-<<<<<<< HEAD
-=======
-      
->>>>>>> ahmed
     </div>
   )
 }
