ab8e61135176faf94cf3e7cc22f5369868f62f67
vickneee <victoria.vavulina@outlook.com>
2024-01-29 10:46:25 +0200

    Footer added

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,8 +2,47 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>
-        Footer
+      <div className="footer">
+          <div className="fLists">
+              <ul className="fList">
+                  <li className="fListItem">Maat</li>
+                  <li className="fListItem">Alueet</li>
+                  <li className="fListItem">Kaupungit</li>
+                  <li className="fListItem">Piirit</li>
+                  <li className="fListItem">Lentokentät</li>
+                  <li className="fListItem">Hotellit</li>
+              </ul>
+              <ul className="fList">
+                  <li className="fListItem">Kodit</li>
+                  <li className="fListItem">Huoneistot</li>
+                  <li className="fListItem">Lomakeskukset</li>
+                  <li className="fListItem">Huvilat</li>
+                  <li className="fListItem">Hostellit</li>
+                  <li className="fListItem">Vierastalot</li>
+              </ul>
+              <ul className="fList">
+                  <li className="fListItem">Ainutlaatuisia majoituspaikkoja</li>
+                  <li className="fListItem">Arvostelut</li>
+                  <li className="fListItem">Matkailuartikkelit</li>
+                  <li className="fListItem">Matkailuyhteisöt</li>
+                  <li className="fListItem">Sesonki- ja lomatarjoukset</li>
+              </ul>
+              <ul className="fList">
+                  <li className="fListItem">Autonvuokraus</li>
+                  <li className="fListItem">Ravintolavaraukset</li>
+                  <li className="fListItem">Matkatoimistot</li>
+              </ul>
+              <ul className="fList">
+                  <li className="fListItem">Asiakaspalvelu</li>
+                  <li className="fListItem">Avoimet työpaikat</li>
+                  <li className="fListItem">Kestävä kehitys</li>
+                  <li className="fListItem">Lehdistökeskus</li>
+                  <li className="fListItem">Turvallisuusresurssikeskus</li>
+                  <li className="fListItem">Sijoittajasuhteet</li>
+                  <li className="fListItem">Käyttöehdot</li>
+              </ul>
+              </div>
+          <div className="fText">Tekijänoikeus © 2024 Helppomatka</div>
     </div>
   );
 };
