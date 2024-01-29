c23c7e343e4356fd3de6629be4ed58624b420a14
vickneee <victoria.vavulina@outlook.com>
2024-01-29 13:49:06 +0200

    Maillist component added

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -1,11 +1,16 @@
 import "./mailList.css"
 
 const MailList = () => {
-  return (
-    <div>
-        Email
-    </div>
-  )
+    return (
+        <div className="mail">
+            <h1 className="mailTitle">Säästä aikaa, säästä rahaa!</h1>
+            <span className="mailDesc">Rekisteröidy ja lähetämme sinulle parhaat tarjoukset</span>
+            <div className="mailInputContainer">
+                <input type="text" placeholder="Sähköpostiosoitteesi"/>
+                <button>Liity jäseneksi</button>
+            </div>
+        </div>
+    )
 }
 
 export default MailList
