8a5f6cddc8fe5998f7d04fb6291d17031638d705
vickneee <victoria.vavulina@outlook.com>
2024-01-31 04:17:59 +0200

    PropertyList pictures added and small corrections

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,11 +1,16 @@
 import "./propertyList.css";
+import MokkitImage from "./images/saad-chaudhry-v9qKtDuq9Lk-unsplash.jpg"
+import HuvilatImage from "./images/john-fornander-y3_AHHrxUBY-unsplash.jpg"
+import LomakeskusImage from "./images/gerson-repreza-CepDpEiALqM-unsplash.jpg"
+import AsuntoImage from "./images/deborah-cortelazzi-gREquCUXQLI-unsplash.jpg"
+import HotelliImage from "./images/markus-spiske-g5ZIXjzRGds-unsplash.jpg"
 
 const PropertyList = () => {
   return (
     <div className="pList">
       <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
+          src={HotelliImage}
           alt=""
           className="pListImg"
         />
@@ -16,7 +21,7 @@ const PropertyList = () => {
       </div>
       <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
+          src={AsuntoImage}
           alt=""
           className="pListImg"
         />
@@ -27,7 +32,7 @@ const PropertyList = () => {
       </div>
       <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg"
+          src={LomakeskusImage}
           alt=""
           className="pListImg"
         />
@@ -38,7 +43,7 @@ const PropertyList = () => {
       </div>
       <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg"
+          src={HuvilatImage}
           alt=""
           className="pListImg"
         />
@@ -49,7 +54,7 @@ const PropertyList = () => {
       </div>
       <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg"
+          src={MokkitImage}
           alt=""
           className="pListImg"
         />
