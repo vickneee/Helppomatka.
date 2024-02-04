755ad613a1787f0ed01e3a05bc7c4875dab64bd1
vickneee <victoria.vavulina@outlook.com>
2024-02-05 00:09:34 +0200

    MailList corrections

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -6,7 +6,7 @@ const MailList = () => {
             <h1 className="mailTitle">Säästäminen on helppo, niin helppo!</h1>
             <span className="mailDesc">Rekisteröidy ja lähetämme sinulle parhaat tarjoukset</span>
             <div className="mailInputContainer">
-                <input type="text" placeholder="Sähköpostiosoitteesi"/>
+                <input type="text" placeholder="Sähköpostiosoitteesi:"/>
                 <button>Liity jäseneksi</button>
             </div>
         </div>
