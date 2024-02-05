9fef0e10b23b913ae7644078f7a629cc1c36ff87
vickneee <victoria.vavulina@outlook.com>
2024-02-05 21:38:11 +0200

    Register corrections

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import "./register.css"
+import React, {useState} from "react";
+import "./../../pages/login/logIn.css"
+import Navbar from "../../components/navbar/Navbar.jsx";
 
 const Register = () => {
     const [email, setEmail] = useState('');
@@ -11,15 +12,24 @@ const Register = () => {
     }
 
     return (
-        <div className="auth-form-container">
-            <h2 className="registerTitle">Rekisteröidy</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">Sähköpostiositteesi</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="sahkopostiosoteesi@gmail.com" id="email" name="email" />
-                <label htmlFor="password">Salasana</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Rekisteröidy</button>
-            </form>
+        <div>
+            <div className="custom-header-main">
+            <div className="custom-header">
+                <Navbar/>
+            </div>
+            </div>
+            <div className="auth-form-container">
+                <h2 className="logInTitle">Rekisteröidy</h2>
+                <form className="login-form" onSubmit={handleSubmit}>
+                    <label htmlFor="email">Sähköpostiositteesi</label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email"
+                           placeholder="sahkopostiosoteesi@gmail.com" id="email" name="email"/>
+                    <label htmlFor="password">Salasana</label>
+                    <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="salasana"
+                           id="password" name="password"/>
+                    <button className="logInBtn" type="submit">Rekisteröidy</button>
+                </form>
+            </div>
         </div>
     )
 }
