2ce312cef35b164f598181c22cfcb736d08f9153
Bee4rt <info.visuel@gmail.com>
2024-02-05 16:40:46 +0200

    added controllers

---
+++ api/controllers/hotelController.js
@@ -0,0 +1,72 @@
+import Hotel from "../models/Hotel.js";
+
+
+
+
+// ===== Create hotel =====
+export const createHotel = async (req, res, next) => {
+    const newHotel = new Hotel(req.body);
+  
+    try {
+      const savedHotel = await newHotel.save();
+      res.status(200).json(savedHotel);
+    } catch (err) {
+      next(err);
+    }
+};
+
+// ===== Update =====
+export const updateHotel = async (req, res, next) => {
+    try {
+      const updatedHotel = await Hotel.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(updatedHotel);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  // ===== delete hotel  =====
+  export const deleteHotel = async (req, res, next) => {
+    try {
+      await Hotel.findByIdAndDelete(req.params.id);
+      res.status(200).json("Hotel has been deleted.");
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  // ===== get hotel =====
+  export const getHotel = async (req, res, next) => {
+    try {
+      const hotel = await Hotel.findById(req.params.id);
+      res.status(200).json(hotel);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  // ===== ALL hotels =====
+  export const getHotels = async (req, res, next) => {
+   Object.keys(req.query).forEach((key) => {
+     if (req.query[key] === "") {
+       delete req.query[key];
+     }
+   });
+  
+   console.log(req.query)
+  
+    const { min, max, ...others } = req.query;
+    try {
+      const hotels = await Hotel.find({
+        ...others,
+        cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+      }).limit(req.query.limit);
+      res.status(200).json(hotels);
+    } catch (err) {
+      next(err);
+    }
+  };
