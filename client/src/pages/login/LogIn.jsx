a50a943d2a6701af66fa9aa100d321de9576c6fb
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-11 16:38:52 +0200

    Login page corrections

--- client/src/pages/login/LogIn.jsx
+++ client/src/pages/login/LogIn.jsx
@@ -3,7 +3,7 @@ import "./logIn.css"
 import Navbar from "../../components/navbar/Navbar.jsx";
 
 const LogIn = () => {
-    const [uname, setUname] = useState('');
+
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
@@ -23,15 +23,12 @@ const LogIn = () => {
                 <h2 className="logInTitle">Kirjaudu sisään</h2>
                 <div className="auth-form-container-2">
                     <form className="login-form" onSubmit={handleSubmit}>
-                        <label htmlFor="text">Käyttäjänimi</label>
-                        <input value={uname} onChange={(e) => setUname(e.target.value)} type="text"
-                               placeholder="käyttäjännimi" id="uname" name="uname"/>
                         <label htmlFor="email">Sähköpostiositteesi</label>
                         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email"
                                placeholder="sahkopostiosoteesi@gmail.com" id="email" name="email"/>
                         <label htmlFor="password">Salasana</label>
-                        <input className="passWord" value={pass} onChange={(e) => setPass(e.target.value)}
-                               type="password" placeholder="salasana"
+                        <input value={pass} onChange={(e) => setPass(e.target.value)} type="password"
+                               placeholder="salasana"
                                id="password" name="password"/>
                         <button className="logInBtn" type="submit">Kirjaudu sisään</button>
                     </form>
