9cab2fe9467d44e501d5631062ff14ff4711566c
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-14 07:10:47 +0200

    MailList corrections

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -1,17 +1,24 @@
-import "./mailList.css"
+import "./mailList.css";
 import { Link } from 'react-router-dom';
+import "react-toastify/dist/ReactToastify.css";
 
 const MailList = () => {
     return (
-        <div className="mail">
-            <h1 className="mailTitle">Säästäminen on helppo, niin helppo!</h1>
-            <span className="mailDesc">Rekisteröidy ja lähetämme sinulle parhaat tarjoukset</span>
-            <div className="mailInputContainer">
-                <input className="inputClass" type="text" placeholder="Sähköpostiosoitteesi"/>
-                <Link to="/register" className="joinBtn">Liity mukaan</Link>
+        <div className="main-container col-md-12 col-sm-6">
+            <div className="mail">
+                <h1 className="mailTitle mb-4">Säästäminen on helppo, niin helppo!</h1>
+                <p className="mailDesc mb-4">Rekisteröidy ja lähetämme sinulle parhaat tarjoukset</p>
+                <div className="mailInputContainer">
+                    <div className="form-group pt-1 mb-4 col-lg-6 col-md-8 col-sm-10">
+                        <input className="form-control form-control-lg" type="text" placeholder="Sähköpostiosoitteesi"/>
+                    </div>
+                    <div className="link-group pt-1 mb-4 col-lg-6 col-md-8 col-sm-10">
+                        <Link to="/login" className="btn mlbtn btn-lg btn-block">Liity mukaan</Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
 
-export default MailList
+export default MailList;
