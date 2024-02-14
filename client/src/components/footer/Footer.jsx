824d93bb757c394e7a0816f06b3346a3a1ce5b78
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-14 09:28:49 +0200

    Footer corrections

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,36 +3,36 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div class="">
-      <footer class="py-3">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-          <li class="nav-item">
+    <div className="">
+      <footer className="py-3">
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+          <li className="nav-item">
             <Link to="/" class="nav-link px-2">
               Etusivu
             </Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <Link to="/" href="#" class="nav-link px-2">
               Asunnot
             </Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <Link to="/" class="nav-link px-2">
               Hotellit
             </Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <Link to="/" class="nav-link px-2">
               Mökkit
             </Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <Link to="/" class="nav-link px-2">
               Huvilat
             </Link>
           </li>
         </ul>
-        <p class="text-center company-name text-muted">&copy; 2024 Helppomatka</p>
+        <p className="text-center company-name text-muted">&copy; 2024 Helppomatka</p>
       </footer>
     </div>
   );
