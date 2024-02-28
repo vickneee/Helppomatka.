867f6c563792eb89a44115b8321b581a091cd0c4
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-28 05:24:15 +0200

    Error Messages

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,9 +1,16 @@
 import User from "../models/User.js";
+import {createError} from "../utils/error.js";
 
 // GET a User
 export const getUser = async (req,res,next)=>{
   try {
     const user = await User.findById(req.params.id);
+
+    // Check if no user was found with the provided ID
+    if (!user) {
+      return next(createError(404, "User not found"));
+    }
+
     res.status(200).json(user);
   } catch (err) {
     next(err);
@@ -14,6 +21,7 @@ export const getUser = async (req,res,next)=>{
 export const getUsers = async (req,res,next)=>{
   try {
     const users = await User.find();
+
     res.status(200).json(users);
   } catch (err) {
     next(err);
@@ -28,6 +36,12 @@ export const updateUser = async (req,res,next)=>{
       { $set: req.body },
       { new: true }
     );
+
+    // Check if no user was found with the provided ID
+    if (!updatedUser) {
+      return next(createError(404, "User not found"));
+    }
+
     res.status(200).json(updatedUser);
   } catch (err) {
     next(err);
@@ -37,7 +51,13 @@ export const updateUser = async (req,res,next)=>{
 // DELETE a User
 export const deleteUser = async (req,res,next)=>{
   try {
-    await User.findByIdAndDelete(req.params.id);
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+    // Check if no user was found
+    if (!deletedUser) {
+      return next(createError(404, "User not found"));
+    }
+
     res.status(200).json("User has been deleted.");
   } catch (err) {
     next(err);
