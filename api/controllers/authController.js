1511033fa1692f2d3b51a959c30e0b881fe5bc96
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-28 03:24:58 +0200

    Commenting code

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -3,19 +3,19 @@ import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
-
-
 export const register = async (req, res, next) => {
     try {
+        // Salting and hashing
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
 
+        // Creating a new user
         const newUser = new User({
             username:req.body.username,
             email:req.body.email,
             password: hash,
       });
-  
+
       await newUser.save();
       res.status(200).send("User has been created.");
     } catch (err) {
@@ -23,24 +23,23 @@ export const register = async (req, res, next) => {
     }
   };
 
-
   export const login = async (req, res, next) => {
     try {
       const user = await User.findOne({ username: req.body.username });
       if (!user) return next(createError(404, "User not found!"));
-  
+
       const isPasswordCorrect = await bcrypt.compare(
         req.body.password,
         user.password
       );
       if (!isPasswordCorrect)
         return next(createError(400, "password or username not correct!"));
-  
+
       const token = jwt.sign(
         { id: user._id, isAdmin: user.isAdmin },
         process.env.JWT
       );
-  
+
       const { password, isAdmin, ...otherDetails } = user._doc;
       res
         .cookie("access_token", token, {
