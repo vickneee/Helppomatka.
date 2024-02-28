0ea8a60445431389c9748423044e7b592bda8ee8
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-28 03:32:08 +0200

    Commenting code

--- api/controllers/roomController.js
+++ api/controllers/roomController.js
@@ -2,15 +2,16 @@ import Room from "../models/Room.js";
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
 
+// CREATE a Room
 export const createRoom = async (req, res, next) => {
     const hotelId = req.params.hotelid;
     const newRoom = new Room(req.body);
-  
+
     try {
       const savedRoom = await newRoom.save();
       try {
         await Hotel.findByIdAndUpdate(hotelId, {
-          $push: { rooms: savedRoom._id }, // this is a mongoDB push methode
+          $push: { rooms: savedRoom._id }, // This is a mongoDB PUSH method
         });
       } catch (err) {
         next(err);
@@ -21,6 +22,27 @@ export const createRoom = async (req, res, next) => {
     }
   };
 
+// GET a Room
+export const getRoom = async (req, res, next) => {
+  try {
+    const room = await Room.findById(req.params.id);
+    res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GRT all Rooms
+export const getRooms = async (req, res, next) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json(rooms);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// UPDATE a Room
   export const updateRoom = async (req, res, next) => {
     try {
       const updatedRoom = await Room.findByIdAndUpdate(
@@ -33,6 +55,26 @@ export const createRoom = async (req, res, next) => {
       next(err);
     }
   };
+
+// DELETE a Room
+export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { rooms: req.params.id },
+      });
+    } catch (err) {
+      next(err);
+    }
+    res.status(200).json("Room has been deleted.");
+  } catch (err) {
+    next(err);
+  }
+};
+
+  // UPDATE Room Availability
   export const updateRoomAvailability = async (req, res, next) => {
     try {
       await Room.updateOne(
@@ -48,36 +90,3 @@ export const createRoom = async (req, res, next) => {
       next(err);
     }
   };
-  export const deleteRoom = async (req, res, next) => {
-    const hotelId = req.params.hotelid;
-    try {
-      await Room.findByIdAndDelete(req.params.id);
-      try {
-        await Hotel.findByIdAndUpdate(hotelId, {
-          $pull: { rooms: req.params.id },
-        });
-      } catch (err) {
-        next(err);
-      }
-      res.status(200).json("Room has been deleted.");
-    } catch (err) {
-      next(err);
-    }
-  };
-  export const getRoom = async (req, res, next) => {
-    try {
-      const room = await Room.findById(req.params.id);
-      res.status(200).json(room);
-    } catch (err) {
-      next(err);
-    }
-  };
-  export const getRooms = async (req, res, next) => {
-    try {
-      const rooms = await Room.find();
-      res.status(200).json(rooms);
-    } catch (err) {
-      next(err);
-    }
-  };
-  
--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,26 @@
 import User from "../models/User.js";
 
+// GET a User
+export const getUser = async (req,res,next)=>{
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+}
+
+// GET all Users
+export const getUsers = async (req,res,next)=>{
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+}
+
+// UPDATE a User
 export const updateUser = async (req,res,next)=>{
   try {
     const updatedUser = await User.findByIdAndUpdate(
@@ -12,26 +33,12 @@ export const updateUser = async (req,res,next)=>{
     next(err);
   }
 }
+
+// DELETE a User
 export const deleteUser = async (req,res,next)=>{
   try {
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json("User has been deleted.");
-  } catch (err) {
-    next(err);
-  }
-}
-export const getUser = async (req,res,next)=>{
-  try {
-    const user = await User.findById(req.params.id);
-    res.status(200).json(user);
-  } catch (err) {
-    next(err);
-  }
-}
-export const getUsers = async (req,res,next)=>{
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
   } catch (err) {
     next(err);
   }
