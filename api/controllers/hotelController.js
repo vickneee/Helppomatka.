39e329347cd87b0d06ef61e5b9e92de36916b670
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-28 03:44:15 +0200

    Commenting code

--- api/controllers/hotelController.js
+++ api/controllers/hotelController.js
@@ -1,34 +1,13 @@
 import Hotel from "../models/Hotel.js";
 import Room from "../models/Room.js";
 
+// CREATE a Hotel
 export const createHotel = async (req, res, next) => {
   const newHotel = new Hotel(req.body);
 
   try {
     const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  } catch (err) {
-    next(err);
-  }
-};
-
-// UPDATE a Hotel
-export const updateHotel = async (req, res, next) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
-    );
-    res.status(200).json(updatedHotel);
-  } catch (err) {
-    next(err);
-  }
-};// DELETE a Hotel
-export const deleteHotel = async (req, res, next) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("Hotel has been deleted.");
+    res.status(200).json(savedHotel); // SAVE a Hotel
   } catch (err) {
     next(err);
   }
@@ -46,13 +25,13 @@ export const getHotel = async (req, res, next) => {
 
 // GET all Hotels
 export const getHotels = async (req, res, next) => {
- Object.keys(req.query).forEach((key) => {
-   if (req.query[key] === "") {
-     delete req.query[key];
-   }
- });
+  Object.keys(req.query).forEach((key) => {
+    if (req.query[key] === "") {
+      delete req.query[key];
+    }
+  });
 
- console.log(req.query)
+  console.log(req.query)
 
   const { min, max, ...others } = req.query;
   try {
@@ -61,6 +40,30 @@ export const getHotels = async (req, res, next) => {
       cheapestPrice: { $gt: min | 1, $lt: max || 999 },
     }).limit(req.query.limit);
     res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// UPDATE a Hotel
+export const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// DELETE a Hotel
+export const deleteHotel = async (req, res, next) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been deleted.");
   } catch (err) {
     next(err);
   }
