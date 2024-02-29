95fbd52f82735e88e65f38844ec9a0571295060f
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-29 07:00:44 +0200

    MailList correction

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -7,15 +7,10 @@ const MailList = () => {
         <div className="main-container col-md-12 col-sm-6">
             <div className="mail">
                 <h1 className="mailTitle mb-4">Säästäminen on helppo, niin helppo!</h1>
-                <p className="mailDesc mb-4">Rekisteröidy ja lähetämme sinulle parhaat tarjoukset</p>
-                <div className="mailInputContainer">
-                    <div className="form-group pt-1 mb-4 col-lg-6 col-md-8 col-sm-10">
-                        <input className="form-control form-control-lg" type="text" placeholder="Sähköpostiosoitteesi"/>
-                    </div>
+                <p className="mailDesc mb-4">Rekisteröidy ja näet aina ensimmäisennä parhaat tarjoukset</p>
                     <div className="link-group pt-1 mb-4 col-lg-6 col-md-8 col-sm-10">
-                        <Link to="/login" className="btn mlbtn btn-lg btn-block">Liity mukaan</Link>
+                        <Link to="/register" className="btn mlbtn btn-lg btn-block">Rekiströidy mukaan tästä</Link>
                     </div>
-                </div>
             </div>
         </div>
     )
