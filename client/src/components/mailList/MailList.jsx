e9b8d8651fc27a00b9216e4184f69dc4c2037b5e
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-02-29 07:39:47 +0200

    MailList correction

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -7,9 +7,9 @@ const MailList = () => {
         <div className="main-container col-md-12 col-sm-6">
             <div className="mail">
                 <h1 className="mailTitle mb-4">Säästäminen on helppo, niin helppo!</h1>
-                <p className="mailDesc mb-4">Rekisteröidy ja näet aina ensimmäisennä parhaat tarjoukset</p>
+                <p className="mailDesc mb-4">Rekisteröidy ja näet aina ensimmäisennä parhaat tarjoukset.</p>
                     <div className="link-group pt-1 mb-4 col-lg-6 col-md-8 col-sm-10">
-                        <Link to="/register" className="btn mlbtn btn-lg btn-block">Rekiströidy mukaan tästä</Link>
+                        <Link to="/register" className="btn mlbtn btn-lg btn-block">Rekiströidy tästä</Link>
                     </div>
             </div>
         </div>
