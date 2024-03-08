c5e9b844d777a434701c282c7e6d5602f60985df
Ahmed <100478516+Bee4rt@users.noreply.github.com>
2024-03-08 23:17:58 +0200

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -50,7 +50,7 @@ const Header = ({ type }) => {
   };
 
   return (
-    <div className={type === "list" ? "header listMode" : "header"}>
+    <div className={type === "list" ? "header otherPages" : "header"}>
       <video autoPlay loop muted>
         <source src={video} type="video/mp4" />
         Your browser does not support the video tag.
