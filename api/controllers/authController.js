08fe7bd8b49146837461ed801b91a77e1d98e7a9
Jose Oldemar Chaves Urbina <72102769+OldemarCRC@users.noreply.github.com>
2024-03-14 09:33:36 +0200

    Update authController.js

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
 import jwt from "jsonwebtoken";
